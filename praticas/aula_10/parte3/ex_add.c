32be343162f0d78cd4ec1c6a9b84aa9a97f07467
Gonçalo <goncalo.lslv.silva@gmail.com>
2022-05-31 22:59:23 +0100

    corrections and added files for class and training

---
+++ praticas/aula_10/parte3/ex_add.c
@@ -0,0 +1,37 @@
+#include <detpic32.h>
+#define K (PBCLK / 1000)
+
+void configUart2(unsigned int baud, char parity, unsigned int stopbits){
+	// Configure BaudRate Generator
+	if(baud >= 600 && baud <= 115200)
+		U2BRG = (PBCLK / (16 * baud)) - 1;	// 	// (PBCLK / (16 * 115200)) - 1
+	else	
+		U2BRG = 10;	// (PBCLK / (16 * 115200)) - 1
+
+	// Configure data bits
+	if(parity == 'E')
+		U2MODEbits.PDSEL = 0b01;	// 8 data bits, even parity -> page 21-6 of UART
+	else if(parity == 'O')
+		U2MODEbits.PDSEL = 0b10;	// 8 data bits, odd parity -> page 21-6 of UART
+	else 
+		U2MODEbits.PDSEL = 0b00;	// 8 data bits, no parity -> page 21-6 of UART
+
+	// configure stop bit
+	if(stopbits == 2)
+		U2MODEbits.STSEL = 1;	// 2 STOP bit -> page 21-6 of UART
+	else
+		U2MODEbits.STSEL = 0; 	// 1 STOP bit -> page 21-6 of UART
+
+	// Enable the trasmitter and receiver modules
+	U2STAbits.UTXEN = 1;	// 3 – Enable the trasmitter -> page 21-8
+	U2STAbits.URXEN = 1;	// 3 - Enable the receiver -> page 21-8
+
+	// Enable UART2
+	U2MODEbits.ON = 1;	// 4 – Enable UART2 (see register U2MODE)
+}
+
+int main(void){
+	configUart2(1, 'N', 1);
+
+	return 0;
+}
